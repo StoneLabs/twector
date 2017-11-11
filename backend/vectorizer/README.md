e3f24415c3ebbbbd58b0f14bed72ac5d3d9ce2f2
StoneInc <git@sobadev.com>
2017-11-11 20:05:33 +0100

    Webhost backend logic prototype (WIP); Vectorizer
    - Removed minor python scripts
    - Further work on webhost logic
    - Added fasttext wrapper bash script
    - Further work on Readme
    - Removed dummy data from vectorizer

--- backend/vectorizer/README.md
+++ backend/vectorizer/README.md
@@ -3,4 +3,4 @@
 This tools are used to vectorize text (e.g. tweets).
 Fasttext is used to vectorized a file using facebooks pretrained models (not inclided in repo, see facebook's fasttext). The following command can be used to read a file in and vectorize every word.
 
-`./fasttext print-word-vectors wiki.xx.bin < test_data.before > test_data.after`
+`text | ./fasttext print-word-vectors wiki.xx.bin`
--- backend/webhost.py
+++ backend/webhost.py
@@ -1,12 +1,46 @@
 from flask import Flask, request, jsonify, make_response
+from scipy import spatial
+import subprocess
+import numpy
 
 app = Flask(__name__)
+
+def getVecForHandle(handle):
+    #Get tweet string=
+    cmd = ['command', 'argument']
+    prc = subprocess.run(cmd, stdout=subprocess.PIPE, input="")
+    tweetdata = prc.stdout.decode('utf-8')
+
+
+    cmd = ['vectorizer/vectorizer', tweetdata]
+    prc = subprocess.run(cmd, stdout=subprocess.PIPE, input="")
+    raw_values = prc.stdout.decode('utf-8')
+
+    #Split by lines to get tweets, then by space to get elements (second dimension)
+    #Remove the first element (word) and convert each element of this sublist to ints
+    raw_elements = np.array([int(i) for i in [line.split(" ")[1:] for line in raw_values.split("\n")]])
+    data_average = raw_elements.mean(axis=0) # Mean over the first dimension (vertical)
+
+    return data_average
+
+
 
 @app.route('/match')
 def predict():
     twitter_handle1 = request.args.get('handle1')
     twitter_handle2 = request.args.get('handle2')
-    r = make_response(jsonify("todo : \"all\"")) # Raw output (including error, success and data field)
+
+    vec1 = getVecForHandle(twitter_handle1)
+    vec2 = getVecForHandle(twitter_handle2)
+
+    cossim = spatial.distance.cosine(vec1, vec2)
+
+    #todo: get tweets 1 u. 2
+    #todo: calc and avrg vectors
+    
+    json_struct = {"math_rate: " : cossim}
+
+    r = make_response(jsonify(json_struct)) # Raw output (including error, success and data field)
     r.headers.set('Access-Control-Allow-Origin', '*')
     return r
 
