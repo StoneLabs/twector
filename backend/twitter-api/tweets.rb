8fd66a50b80b80bb2929c900d75324f64e279b8d
sesshomariu <sesshotv@t-online.de>
2017-11-11 19:25:03 +0100

    Refreshed twitter-api scripts

--- backend/twitter-api/tweets.rb
+++ backend/twitter-api/tweets.rb
@@ -5,32 +5,23 @@ require "net/http"
 require "json"
 require "oauth"
 require "cgi"
+require "./lib.rb"
 
 ARGV[0] || abort("ERROR[#{__FILE__}]: NO HANDLE PROVIDED\nUSAGE: ruby #{__FILE__} <handle>")
 handle = ARGV[0]
 $auth = %x(source #{Dir.pwd}/auth.sh; echo $CONSUMER_KEY $CONSUMER_SECRET $API_KEY $API_SECRET).chomp.split
-
-def oauth_get(_url)
-	url = URI(_url)
-	http = Net::HTTP.new(url.host, url.port)
-	http.use_ssl = true
-	http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-	request = Net::HTTP::Get.new(url.request_uri)
-	request.oauth!( http, OAuth::Consumer.new($auth[0], $auth[1]), OAuth::Token.new($auth[2], $auth[3]) )
-	response = nil ; http.start{response=http.request(request)}
-	raise("HTTP Error #{response.code}") if response.code[0]!='2'
-	return JSON.parse response.body
-end
 
 tweets = []
 
 last_id = 0
 
 10.times do
-	res = oauth_get("https://api.twitter.com/1.1/statuses/user_timeline.json?" + {
-		"screen_name" => "fluffyizu",
+	res = Twector::oauth_get("https://api.twitter.com/1.1/statuses/user_timeline.json?" + {
+		"screen_name" => handle,
 		"count" => "2000"
 	}.to_a.map{|i|[CGI::escape(i[0]),CGI::escape(i[1])].join(?=)}.join(?&))
 	tweets += res
 	last_id = res.map{|i|i["id"]}.max
 end
+
+$stdout.puts JSON.dump tweets
